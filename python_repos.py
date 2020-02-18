587967cf6e4a2c47265f9b06350d980ae55260b2
handes <2451322819@qq.com>
2020-02-18 15:58:25 +0800

    2020/2/18

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # 执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -13,7 +15,7 @@ print("Total repositories:", response_dict['total_count'])
 
 #探索有关仓库的信息
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
+# print("Repositories returned:", len(repo_dicts))
 
 # 研究第一仓库
 # repo_dict = repo_dicts[0]
@@ -30,11 +32,25 @@ print("Repositories returned:", len(repo_dicts))
 # print('Updated:', repo_dict['updated_at'])
 # print('Description:', repo_dict['description'])
 
-print("\nSelected information about each repository:")
+# print("\nSelected information about each repository:")
+# for repo_dict in repo_dicts:
+#     print('\nName:', repo_dict['name'])
+#     print('Owner:', repo_dict['owner']['login'])
+#     print('Stars:', repo_dict['stargazers_count'])
+#     print('Repository:', repo_dict['html_url'])
+#     print('Description:', repo_dict['description'])
+
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print('\nName:', repo_dict['name'])
-    print('Owner:', repo_dict['owner']['login'])
-    print('Stars:', repo_dict['stargazers_count'])
-    print('Repository:', repo_dict['html_url'])
-    print('Description:', repo_dict['description'])
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+#可视化
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projets on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
 
